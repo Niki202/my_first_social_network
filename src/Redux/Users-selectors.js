f6791df25de44156dedb726c5a082128498f0953
dmitriynikitin <dmitriynikitin2022@gmail.com>
2023-01-29 17:14:05 +0300

    Добавлена библиотека reselect в компоненте users +комментарии

--- src/Redux/Users-selectors.js
+++ src/Redux/Users-selectors.js
@@ -1,11 +1,15 @@
 import {createSelector} from "reselect";
 
+// Примитивная функция для доставания данных из стейта
 const getUsers = (state) => {
     return state.usersPage.users
 }
 
+// селектор запускается только когда меняются входные данные из getUsers
+// иначе он предоставляет сохраненный результат
 export const getUsersSel = createSelector(getUsers, (users) => {
     console.log('calculate')
+    // Код который перерабатывает данные из стейта но не срабатывает при каждой перерисовке
     return users.filter(u => true)
 })
 
